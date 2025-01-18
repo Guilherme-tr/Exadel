74a9d93d0afe141cfa40305a7e31bdc78f8c91e0
Guilherme Trujilo <guilherme.trujilo@altbank.co>
2025-01-18 18:28:32 -0300

    feat: create update status endpoint

--- src/main/java/com/exadel/microservice/controller/OrderController.java
+++ src/main/java/com/exadel/microservice/controller/OrderController.java
@@ -3,14 +3,14 @@ package com.exadel.microservice.controller;
 import com.exadel.microservice.dto.OrderRequest;
 import com.exadel.microservice.entity.Order;
 import com.exadel.microservice.service.OrderService;
+import com.exadel.microservice.util.OrderStatus;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/orders")
@@ -31,5 +31,11 @@ public class OrderController {
                                 .buildAndExpand(newOrder.getId())
                                         .toUri();
         return ResponseEntity.created(location).body(newOrder);
+    }
+
+    @PatchMapping("/{orderId}/status")
+    public ResponseEntity<Order> updateOrderStatus(@PathVariable UUID orderId, @RequestParam OrderStatus newStatus){
+        Order updatedOrder = orderService.updateOrderStatus(orderId, newStatus);
+        return ResponseEntity.ok(updatedOrder);
     }
 }
